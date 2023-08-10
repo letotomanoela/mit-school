8ff7f93f37659ace597ad0fa9d8077f1af67353f
Letoto Manoela <100037690+letotomanoela@users.noreply.github.com>
2023-08-10 09:25:15 +0300

    Update Stats.js

--- back-end/routes/Stats.js
+++ back-end/routes/Stats.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { totalNumber } from "../controllers/stats.controller";
+import { totalNumber } from "../controllers/stats.controller.js";
 
 const router = Router();
 
