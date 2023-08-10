24c34d7e1f60f995d2fdfe4cd9b5058531f9dbc0
Letoto Manoela <100037690+letotomanoela@users.noreply.github.com>
2023-08-10 09:58:51 +0300

    Update LineChart.jsx

--- front-end/src/Components/Chart/LineChart.jsx
+++ front-end/src/Components/Chart/LineChart.jsx
@@ -1,16 +1,9 @@
-import Chart from "react-apexcharts";
 
 const LineChart = (props) => {
   const { series, options } = props;
 
   return (
-    <Chart
-      options={options}
-      type="area"
-      width="100%"
-      height="80%"
-      series={series}
-    />
+    <>hello</>
   );
 };
 
